32c1be8d16a0be2f013822ebc636e7568d974723
KirkChen <kirkchen@kkbox.com>
2017-09-23 10:18:27 +0800

    抽出 rate 變數，統一折扣百分比

--- src/shoppingCart.js
+++ src/shoppingCart.js
@@ -3,13 +3,16 @@ function ShoppingCart() {
 
 ShoppingCart.prototype.Calculate = function (level, price, qty) {
     var totalPrice = price * qty;
+    var rate = 1;
+
     if (level === 'VIP' && totalPrice > 500) {
-        return totalPrice * 0.8;
+        rate = 0.8;
     }
     else if (level === 'Normal' && totalPrice > 1000 && qty > 3) {
-        return totalPrice * 0.85;
+        rate = 0.85
     }
-    return totalPrice;
+
+    return totalPrice * rate;
 }
 
 module.exports = ShoppingCart;
