965129eb110ba5813c35a5bd0ae2adc090e09ad9
KirkChen <kirkchen@kkbox.com>
2017-09-23 10:31:23 +0800

    建立 Rule 規則，使用 filter 判斷 Rule

--- src/shoppingCart.js
+++ src/shoppingCart.js
@@ -1,4 +1,8 @@
 function ShoppingCart() {
+    this.rules = [
+        { level: 'VIP', priceGate: 500, qtyGate: 0, rate: 0.8 },
+        { level: 'Normal', priceGate: 1000, qtyGate: 3, rate: 0.85 }
+    ]
 }
 
 ShoppingCart.prototype.Calculate = function (level, price, qty) {
@@ -11,14 +15,20 @@ ShoppingCart.prototype.Calculate = function (level, price, qty) {
 ShoppingCart.prototype.GetRate = function (level, totalPrice, qty) {
     var rate = 1;
 
-    if (level === 'VIP' && totalPrice > 500 && qty > 0) {
-        rate = 0.8;
-    }
-    else if (level === 'Normal' && totalPrice > 1000 && qty > 3) {
-        rate = 0.85
+    var matchedRule = this.rules.filter(
+        (rule) => this.IsMatchRule(rule, level, totalPrice, qty));
+    if (matchedRule.length > 0) {
+        return matchedRule[0].rate;
     }
 
     return rate;
 }
 
+ShoppingCart.prototype.IsMatchRule = function(rule, level, totalPrice, qty) {
+    return level === rule.level &&
+        totalPrice > rule.priceGate &&
+        qty > rule.qtyGate;
+}
+
 module.exports = ShoppingCart;
+
