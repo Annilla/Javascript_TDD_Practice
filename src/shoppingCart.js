c002530dfc1ec2d2a80b3b5bb01bea6d22437208
KirkChen <kirkchen@kkbox.com>
2017-09-23 10:24:06 +0800

    抽出取得折扣百分比的 function

--- src/shoppingCart.js
+++ src/shoppingCart.js
@@ -3,16 +3,22 @@ function ShoppingCart() {
 
 ShoppingCart.prototype.Calculate = function (level, price, qty) {
     var totalPrice = price * qty;
+    var rate = this.GetRate(level, totalPrice, qty);
+
+    return totalPrice * rate;
+}
+
+ShoppingCart.prototype.GetRate = function (level, totalPrice, qty) {
     var rate = 1;
 
-    if (level === 'VIP' && totalPrice > 500) {
+    if (level === 'VIP' && totalPrice > 500 && qty > 0) {
         rate = 0.8;
     }
     else if (level === 'Normal' && totalPrice > 1000 && qty > 3) {
         rate = 0.85
     }
 
-    return totalPrice * rate;
+    return rate;
 }
 
 module.exports = ShoppingCart;
