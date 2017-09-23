9267ace4be092f06c62a4e76c5fa7a3a2818277b
KirkChen <kirkchen@kkbox.com>
2017-09-23 09:59:55 +0800

    實現一般會員，超過 1000 元時，會打 85 折

--- src/shoppingCart.js
+++ src/shoppingCart.js
@@ -2,11 +2,14 @@ function ShoppingCart() {
 }
 
 ShoppingCart.prototype.Calculate = function (level, price, qty) {
-   var totalPrice = price * qty;
-   if(level === 'VIP' && totalPrice > 500){
-       return totalPrice * 0.8;
-   }
-   return totalPrice; 
+    var totalPrice = price * qty;
+    if (level === 'VIP' && totalPrice > 500) {
+        return totalPrice * 0.8;
+    }
+    else if (level === 'Normal' && totalPrice > 1000) {
+        return totalPrice * 0.85;
+    }
+    return totalPrice;
 }
 
 module.exports = ShoppingCart;
