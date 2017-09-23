0482faeded8bcc80816198246273b785cbec8ac9
KirkChen <kirkchen@kkbox.com>
2017-09-23 09:54:52 +0800

    實現 VIP 會員折扣邏輯

--- src/shoppingCart.js
+++ src/shoppingCart.js
@@ -2,7 +2,11 @@ function ShoppingCart() {
 }
 
 ShoppingCart.prototype.Calculate = function (level, price, qty) {
-   return price * qty; 
+   var totalPrice = price * qty;
+   if(totalPrice > 500){
+       return totalPrice * 0.8;
+   }
+   return totalPrice; 
 }
 
 module.exports = ShoppingCart;
