8528824e498cdfe2ea8c47d591b68c373fb1e06a
KirkChen <kirkchen@kkbox.com>
2017-09-23 10:33:38 +0800

    新增 預設 Rule

--- src/shoppingCart.js
+++ src/shoppingCart.js
@@ -1,7 +1,8 @@
 function ShoppingCart() {
     this.rules = [
-        { level: 'VIP', priceGate: 500, qtyGate: 0, rate: 0.8 },
-        { level: 'Normal', priceGate: 1000, qtyGate: 3, rate: 0.85 }
+        { level: 'VIP', priceGate: 500, qtyGate: 0, rate: 0.8, isDefault: false },
+        { level: 'Normal', priceGate: 1000, qtyGate: 3, rate: 0.85, isDefault: false },
+        { rate: 1, isDefault: true }
     ]
 }
 
@@ -13,21 +14,14 @@ ShoppingCart.prototype.Calculate = function (level, price, qty) {
 }
 
 ShoppingCart.prototype.GetRate = function (level, totalPrice, qty) {
-    var rate = 1;
-
-    var matchedRule = this.rules.filter(
-        (rule) => this.IsMatchRule(rule, level, totalPrice, qty));
-    if (matchedRule.length > 0) {
-        return matchedRule[0].rate;
-    }
-
-    return rate;
+    var matchedRule = this.rules.filter((rule) => this.IsMatchRule(rule, level, totalPrice, qty));
+    return matchedRule[0].rate;
 }
 
-ShoppingCart.prototype.IsMatchRule = function(rule, level, totalPrice, qty) {
-    return level === rule.level &&
+ShoppingCart.prototype.IsMatchRule = function (rule, level, totalPrice, qty) {
+    return (level === rule.level &&
         totalPrice > rule.priceGate &&
-        qty > rule.qtyGate;
+        qty > rule.qtyGate) || rule.isDefault;
 }
 
 module.exports = ShoppingCart;
